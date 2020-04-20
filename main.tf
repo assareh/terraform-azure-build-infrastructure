6be453ec47d239784ea8046d82a227defecd09a5
Andy Assareh <assareh@me.com>
2020-04-20 15:31:27 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,78 @@
+provider "azurerm" {
+  version = "=1.44.0"
+}
+
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-resource-group"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.myresourcegroup.location
+  address_space       = [var.address_space]
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "${var.prefix}-subnet"
+  # for_each = var.subnets...
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
+  address_prefix       = var.subnet_prefix
+}
+
+resource "azurerm_network_security_group" "catapp-sg" {
+  name                = "${var.prefix}-sg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_resource_group" "anotherresourcegroup" {
+  name     = "${var.prefix}-resource-group-2"
+  location = var.location
+}
+
+output rg1 {
+  value = azurerm_resource_group.myresourcegroup.id
+}
+
+output rg2 {
+  value = azurerm_resource_group.anotherresourcegroup.id
+}
