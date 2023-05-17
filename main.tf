cc5741c459f1b5c5ca1532fdaf715eab177a14af
Andy Assareh <assareh@hashicorp.com>
2023-05-16 20:35:01 -0700

    restored full module

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-resource "null_resource" "local-exec" {
-  triggers = {
-    build_number = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "env"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
@@ -16,49 +7,49 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-# resource "azurerm_network_security_group" "example" {
-#   name                = "example-security-group"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-# }
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-# resource "azurerm_virtual_network" "example" {
-#   name                = "example-network"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   address_space       = ["10.0.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-#   subnet {
-#     name           = "subnet1"
-#     address_prefix = "10.0.1.0/24"
-#   }
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 
-#   subnet {
-#     name           = "subnet2"
-#     address_prefix = "10.0.2.0/24"
-#     security_group = azurerm_network_security_group.example.id
-#   }
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
 
-#   tags = {
-#     environment = "Production"
-#   }
-# }
+  tags = {
+    environment = "Production"
+  }
+}
 
-# data "azurerm_subnet" "example" {
-#   name                 = "subnet1"
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   resource_group_name  = azurerm_resource_group.example.name
-# }
+data "azurerm_subnet" "example" {
+  name                 = "subnet1"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+}
 
-# output rg_name {
-#   value = azurerm_resource_group.example.name
-# }
+output rg_name {
+  value = azurerm_resource_group.example.name
+}
 
-# output subnet_name {
-#   value       = data.azurerm_subnet.example.name
-# }
+output subnet_name {
+  value       = data.azurerm_subnet.example.name
+}
 
-# output vnet_name {
-#   value       = azurerm_virtual_network.example.name
-# }
+output vnet_name {
+  value       = azurerm_virtual_network.example.name
+}
